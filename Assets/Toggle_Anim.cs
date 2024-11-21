8f159bb92635e24400195115a07de5ea687910f3
Stephen Roebuck <shroebuck@gmail.com>
2024-11-21 08:39:35 -0800

    Odin Import + VFX Animations.

---
+++ Assets/Toggle_Anim.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Effects { BITE, IMPACT, LIGHTNING_AURA, MAGIC_SPAWN, SMOKE_BOMB, SPLATTER, TOON, WARP }
+
+public class Toggle_Anim : MonoBehaviour
+{
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
