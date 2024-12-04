f83705b8d08e8c788b93ae8c7e8c8ab9c3720cb1
Stephen Roebuck <shroebuck@gmail.com>
2024-12-04 09:32:53 -0800

    More Animations.

--- Assets/Toggle_Anim.cs
+++ Assets/Toggle_Anim.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEditor.Animations;
 using UnityEngine;
 
-public enum Effects { BITE, IMPACT, LIGHTNING_AURA, MAGIC_SPAWN, SMOKE_BOMB, SPLATTER, TOON, WARP }
+public enum Effects { BITE = 0, IMPACT, LIGHTNING_AURA, MAGIC_SPAWN, SMOKE_BOMB, SPLATTER, TOON, WARP }
+public enum Spells { ABSORB = 0, FIRESPELL, WINDSPELL, ICESPELL, SHATTER, LIGHTNING, CLAW, EARTHSPIKES, POISON }
 
 public class Toggle_Anim : MonoBehaviour
 {
-    
+    public Animator animator;
+    public Effects currentEffect;
+    public Spells currentSpell;
 
+    public AnimatorController effectAnim, spellAnim;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Button("Play Effect")]
+    public void PlayEffect() {
+        animator.runtimeAnimatorController = effectAnim;
+        animator.SetTrigger(currentEffect.ToString());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [Button("Play Spell")]
+    public void PlaySpell() {
+        animator.runtimeAnimatorController = spellAnim;
+        animator.SetTrigger(currentSpell.ToString());
     }
 }
